78dd69153db306652e99d5a93135d9860d15a971
skmbw <tongku2008@126.com>
2018-09-19 14:59:58 +0800

    java的测试代码

--- geomesa-examples-spark/src/main/scala/com/example/geomesa/spark/SparkJavaTest.java
+++ geomesa-examples-spark/src/main/scala/com/example/geomesa/spark/SparkJavaTest.java
@@ -1,6 +1,16 @@
 package com.example.geomesa.spark;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.spark.SparkConf;
+import org.apache.spark.SparkContext;
+import org.geotools.data.Query;
+import org.locationtech.geomesa.spark.GeoMesaSpark;
+import org.locationtech.geomesa.spark.SpatialRDD;
+import org.locationtech.geomesa.spark.hbase.HBaseSpatialRDDProvider;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author yinlei
@@ -9,5 +19,15 @@ import org.apache.spark.SparkConf;
 public class SparkJavaTest {
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf();
+        sparkConf.setMaster("spark://10.0.12.145:7077").setAppName("appName");
+
+        SparkContext sparkContext = SparkContext.getOrCreate(sparkConf);
+
+        Map<String, Serializable> params = new HashMap<>();
+        HBaseSpatialRDDProvider rddProvider = (HBaseSpatialRDDProvider) GeoMesaSpark.apply(params);
+        // 和params是相同的
+        scala.collection.immutable.Map<String, String> immap = new scala.collection.immutable.HashMap<>();
+        SpatialRDD spatialRDD = rddProvider.rdd(new Configuration(), sparkContext, immap, new Query());
+
     }
 }
